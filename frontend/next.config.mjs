fe9df2dbe8df9776d94461a3b73cc6349e61cfcf
Akash Saikia <arsaikia26@gmail.com>
2026-02-08 22:10:41 +0530

    changes in nextjsconfig

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-    domains: ["localhost" , "images.unsplash.com" ,"deih43ym53wif.cloudfront.net" ,"res.cloudinary.com" ], // Add your backend host domain here
+    domains: ["localhost" , "images.unsplash.com" ,"deih43ym53wif.cloudfront.net" ,"res.cloudinary.com" , "plus.unsplash.com",  ], // Add your backend host domain here
   },
 };
 
